cd458ca94ceaf1788ff34267149c00fdfc7d0957
Ram Singla <ram.singla@gmail.com>
2010-01-18 18:07:23 +0530

    Story Group Fix

--- app/controllers/story_groups_controller.rb
+++ app/controllers/story_groups_controller.rb
@@ -3,12 +3,12 @@ class StoryGroupsController < ApplicationController
   layout 'scaffold'
   
   def index
-    @story_groups = StoryGroup.current_session.paginate( :page => params[:page] || '1', :include => [ :language, :top_story ] )
+    @story_groups = StoryGroup.current_session.paginate( :page => params[:page] || '1', :include => [ :language, :top_stories ] )
   end
 
   def show
     @story_group = StoryGroup.find( params[:id] )
-    @stories = @story_group.stories.paginate( :page => params[:page] || '1' )
+    @stories = @story_group.stories.paginate( :page => params[:page] || '1', :include => :story_metric )
   end
 
 end
