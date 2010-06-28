216e9d40f1f3bd75112526924fbd265c31fe71a1
Ram Singla <ram.singla@gmail.com>
2010-06-28 12:07:25 +0530

    Slow Duplicate Query Fix

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -5,7 +5,7 @@ class StoriesController < ApplicationController
   def show
     @story = Story.find( params[:id], :include => [:story_content, :authors, :source] )
     @group = @story.story_groups.current_session.first
-    story_metrics = StoryMetric.find( :all, :conditoins => { :master_id => @story.id }, :include => { :stories => :source } )
+    story_metrics = StoryMetric.find( :all, :conditions => { :master_id => @story.id }, :include => { :stories => :source } )
     @duplicates = story_metrics.collect( &:story )
     # @duplicates = Story.all(  :conditions => 
     #     [ 'stories.id IN ( SELECT story_id FROM story_metrics 
