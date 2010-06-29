23de15990a48029f9356a8d5c0d8bfe3e784deb7
Ram Singla <ram.singla@gmail.com>
2010-06-29 17:37:13 +0530

    Queue Sizes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,8 @@ class DashboardController < ApplicationController
   layout 'scaffold'
   
   def index
+    @duplicate_checker_queue_size = Story.count( :all, :conditions => { :duplicate_checked => false } )
+    @quality_ratings_queue_size = Story.count( :all, :conditions => { :duplicate_checked => true, :quality_ratings_generated => false } )
   end
 
 end
